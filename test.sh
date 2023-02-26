11364daf30c1527c1c8da79c53fd31c69a1cd05c
kevinhooke <kevin.hooke@gmail.com>
2023-02-26 12:51:30 -0800

    updated handler to pipe multiple commands to process

--- test.sh
+++ test.sh
@@ -5,15 +5,17 @@ function handler () {
 
   #parse the move value and emit as raw unquoted text
   MOVE=$(echo "$EVENT_DATA" | jq -r .move)
+  USERID=$(echo "$EVENT_DATA" | jq -r .userid)
 
   #TODO load game save for current user, save after move
   #to save, pipe: save\nfilename
   #to load, pipe: restore\nfilename
-  OUTPUT=$(echo "$MOVE" | dfrotz zork1.z3)
+  # (echo "N"; echo "save"; echo "multiple.qzl")
+  OUTPUT=$( (echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl";) | dfrotz zork1.z3)
   echo "$OUTPUT" 1>&2;
-  RESPONSE="{ \"result\" : \"${OUTPUT}\" }"
+  RESPONSE="{ \"result\" : \"$OUTPUT\" }"
 
-  # is aws cli present?
-  aws s3 cp /tmp/test.zql s3://zmachine-bot-savegames/test.zql
+  # use aws cli to copy savegame file from /tmp/$USERID.qzl to s3 bucket
+  aws s3 cp /tmp/$USERID.qzl s3://zmachine-bot-savegames/$USERID.qzl
   echo $RESPONSE
 }
