830206a043fe3e78b14d524bfa839c25d5008220
kevinhooke <kevin.hooke@gmail.com>
2023-03-07 08:13:10 -0800

    added loading game save to frotz wrapper

--- test.sh
+++ test.sh
@@ -10,8 +10,17 @@ function handler () {
   #TODO load game save for current user, save after move
   #to save, pipe: save\nfilename
   #to load, pipe: restore\nfilename
-  # (echo "N"; echo "save"; echo "multiple.qzl")
-  OUTPUT=$( (echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl"; echo "quit"; echo "";) | dfrotz zork1.z3)
+  
+  #TODO attempt load of previous game save
+  # use aws cli to copy savegame file from /tmp/$USERID.qzl to s3 bucket
+  S3OUTPUT=$(aws s3 cp s3://zmachine-bot-savegames/$USERID.qzl /tmp/$USERID.qzl)
+
+  #first game save:
+  #>Please enter a filename [zork1.qzl]: Ok.\n\n>There was no verb in that sentence!\
+
+  #subsequent game save
+  # "Overwrite existing file?" : echo "Y";
+  OUTPUT=$( (echo "restore"; echo "/tmp/$USERID.qzl"; echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl"; echo "Y"; echo "quit"; echo "";) | dfrotz zork1.z3)
   echo "$OUTPUT" 1>&2;
   RESPONSE="{ \"result\" : \"$OUTPUT\" }"
 
