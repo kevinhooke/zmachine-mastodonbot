e8d1c0ac413a189718256c058bc8e45e32585358
kevinhooke <kevin.hooke@gmail.com>
2023-03-09 00:25:02 -0800

    aded conditional handling of save game file

--- test.sh
+++ test.sh
@@ -11,16 +11,22 @@ function handler () {
   #to save, pipe: save\nfilename
   #to load, pipe: restore\nfilename
   
-  #TODO attempt load of previous game save
   # use aws cli to copy savegame file from /tmp/$USERID.qzl to s3 bucket
-  S3OUTPUT=$(aws s3 cp s3://zmachine-bot-savegames/$USERID.qzl /tmp/$USERID.qzl)
+  # if s3 cp doesn't find the file, set S3OUTPUT to "no-save-file" to skip loading the save game
+  S3OUTPUT=$(aws s3 cp s3://zmachine-bot-savegames/$USERID.qzl /tmp/$USERID.qzl || echo "no-save-file")
 
   #first game save:
   #>Please enter a filename [zork1.qzl]: Ok.\n\n>There was no verb in that sentence!\
-
   #subsequent game save
   # "Overwrite existing file?" : echo "Y";
-  OUTPUT=$( (echo "restore"; echo "/tmp/$USERID.qzl"; echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl"; echo "Y"; echo "quit"; echo "";) | dfrotz zork1.z3)
+
+  #if game save file not found, start game with the move command, otherwise load the file
+  if [[ $S3OUTPUT == "no-save-file" ]]
+  then
+    OUTPUT=$( (echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl"; echo "Y"; echo "quit"; echo "";) | dfrotz zork1.z3)
+  else
+    OUTPUT=$( (echo "restore"; echo "/tmp/$USERID.qzl"; echo "$MOVE"; echo "save"; echo "/tmp/$USERID.qzl"; echo "Y"; echo "quit"; echo "";) | dfrotz zork1.z3)
+  fi
   echo "$OUTPUT" 1>&2;
   RESPONSE="{ \"result\" : \"$OUTPUT\" }"
 
